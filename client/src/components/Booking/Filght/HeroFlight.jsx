320f71dad31b11b84fba74bbf81cd1454edff035
RaushanKumar606 <roshankumarsingh964@gmail.com>
2025-01-07 23:33:48 +0530

    new add

--- client/src/components/Booking/Filght/HeroFlight.jsx
+++ client/src/components/Booking/Filght/HeroFlight.jsx
@@ -1,8 +1,23 @@
 
 function HeroFlight() {
   return (
-    <div>
-      <h1>Hero flight</h1>
+    <div className="flight-container">
+      <div className="flight-header">
+      <div className="left-img">
+          <img
+            src="assets/countery/luxruyHotel.jpg"
+            alt="Hotel"
+            className="img-fluid"
+          />
+        </div>
+        <div className="right-head">
+          <h1>Find the top Hotels nearby.</h1>
+          <p>
+          We bring you not only a stay option, but an experience in your budget to enjoy the luxury
+          </p>
+        </div>
+      </div>
+      
     </div>
   )
 }
